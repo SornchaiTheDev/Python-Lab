0b66f9dd106e03352b7db8fd658dea6e0cc917ca
Sornchai Somsakul <54209864+SornchaiTheDev@users.noreply.github.com>
2022-08-01 17:09:58 +0700

    Update : 05 problem

--- 05 Triangle of 1s.py
+++ 05 Triangle of 1s.py
@@ -1,29 +1,27 @@
-height = int(input())
-i = 1
-while i <= height:
-    if i == 1:
-        print(f"{1:>11}")
-    elif i == 2:
-        print(f"{1:>9}   {1:1}")
-    elif i == 3:
-        print(f"{1:>7}   {1:>5}")
-    elif i == 4:
-        print(f"{1:>5}   {1:>9}")
-    elif i == 5:
-        print(f"{1:>3}   {1:>13}")
-    elif i == 6:
-        print(f"{1:>1}   {1:>17}")
+height = int(input("Enter height: "))
+space = 3
+if height > 0:
+    i = height * 2 - 2
+    while i >= 0:
+        if i == height * 2 - 2:
+            print(" " * i, end="")
+            print(1)
+        else:
+            print(" " * i, end="")
+            print(1, end="")
+            print(" " * space, end="")
+            print(1)
 
-    i += 1
+            space += 4
+        i -= 2
 
-# [ ] หา pattern  ต่อ
 
 """
 
-          1           (space = 8)
-        1   1         (space = 6) (between = 3) 
-      1       1       (space = 4) (between = 7)
-    1           1     (space = 2) (between = 11)
-  1               1   (space = 0) (between = 15)
-1                   1
+          1           (space = 10) (between = 0)
+        1   1         (space = 8)  (between = 3) 
+      1       1       (space = 6)  (between = 7)
+    1           1     (space = 4)  (between = 11)
+  1               1   (space = 2)  (between = 15)
+1                   1 (space = 0)  (between = 19)
 """
