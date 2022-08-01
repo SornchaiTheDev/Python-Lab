3b81eae02bd8cf9b07885c1e48f8c1a5bd083e8e
Sornchai Somsakul <54209864+SornchaiTheDev@users.noreply.github.com>
2022-08-01 16:18:38 +0700

    Update : 06 problem

--- 06 กฎของเก้า.py
+++ 06 กฎของเก้า.py
@@ -1,14 +1,15 @@
-import math
-number = input()
-digits = len(number)
+number = int(input())
 i = 1
 sum = 0
-while i <= digits:
-    digit = math.floor(int(number) % 10)
+while True:
+    if number == 0:
+        break
+    digit = number % 10
     sum += digit
-    number = int(number) / 10
+    number = number // 10
     i += 1
-if int(number) % 9 == sum % 9 :
+
+if  number % 9 == sum % 9:
     print(f"Yes {sum}")
 else:
     print(f"No {sum % 9}")
