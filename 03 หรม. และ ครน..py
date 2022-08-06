9bef5f9d4a6f156839ff3973ab3bb2b9c50bab1d
Sornchai Somsakul <54209864+SornchaiTheDev@users.noreply.github.com>
2022-08-07 00:04:07 +0700

    Add : 03

--- 03 หรม. และ ครน..py
+++ 03 หรม. และ ครน..py
@@ -1,4 +1,26 @@
 firstNumber = int(input("Enter the first number: "))
 secondNumber = int(input("Enter the second number: "))
 
-print(f"  >> gcd({firstNumber}, {secondNumber}) =      4")
+first = firstNumber
+second = secondNumber
+gcd = 1
+if first == second :
+    gcd = first
+else :
+    while True:
+        
+        mod = first % second
+        if mod > 0:
+            first = second
+            second = mod
+        else:
+            break
+        gcd = mod
+
+lcm = gcd
+lcm *= firstNumber // gcd
+lcm *= secondNumber // gcd
+
+
+print(f"  >> gcd({firstNumber}, {secondNumber}) ={gcd:>7d}")
+print(f"  >> lcm({firstNumber}, {secondNumber}) ={lcm:>7d}")
