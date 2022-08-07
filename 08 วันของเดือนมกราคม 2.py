8ea578b4be15af707e99776ade8e2eff58f23e0f
Sornchai Somsakul <54209864+SornchaiTheDev@users.noreply.github.com>
2022-08-07 16:51:47 +0700

    Add : 08

--- 08 วันของเดือนมกราคม 2.py
+++ 08 วันของเดือนมกราคม 2.py
@@ -1,3 +1,12 @@
+"""
+Mon Tues Wed Thurs  Fri Sat Sun
+30    31  1    2     3   4   5
+6     7   8    9    10  11   12
+13   14   15  16    17  18   19
+
+ 
+"""
+
 day = input()
 n = int(input())
 if day != "Sunday" and day != "Monday" and day != "Tuesday" and day != "Wednesday" and day != "Thursday" and day != "Friday" and day != "Saturday" or n <= 0 or n > 31:
@@ -17,10 +26,10 @@ else:
         day = 5
     elif day == "Saturday":
         day = 6
-        
-    print(day)
+    
 
-    Day = (n - day) % 7
+    Day = (n - 1 + day ) % 7
+
     if Day == 0:
         print("Sunday")
     elif Day == 1:
